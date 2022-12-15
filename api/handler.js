1bc16842dcb6301b1edf4a873afa5172cc4dd40a
EmmanueleD <emmanuele.online@gmail.com>
2022-12-15 18:32:46 -0300

    dsw

--- api/handler.js
+++ api/handler.js
@@ -2,14 +2,13 @@ import axios from 'axios'
 export default async function handler(req, res) {
 
   console.log('sto chiamando mail chimp')
-  const mailchimp = await axios.get(process.env.VUE_APP_MAIL_CHIMP_API_BASE, {
+  const mailchimp = await axios.get(process.env.VUE_APP_MAIL_CHIMP_API_BASE+'/members?count=100', {
     headers:{
       'Authorization' : 'Bearer ' + process.env.VUE_APP_MAIL_CHIMP_BEARER
     }
   })
-  .then(result => result)
+  .then(result => result.data)
   .finally(()=> console.log('sono uscito da mailchimp'))
-
 
   return res.send(mailchimp);
 }
