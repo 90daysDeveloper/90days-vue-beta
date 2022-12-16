7055c78363067f44fffa53d1cb5ee6875226d753
Camilla Tofani <tech@90days.it>
2022-12-16 23:19:14 +0100

    dsw

--- api/postmember.js
+++ api/postmember.js
@@ -1,24 +1,6 @@
 import axios from 'axios'
 export default async function postmember(req, res) {
-	const mailchimp = await axios.post(process.env.VUE_APP_MAIL_CHIMP_API_BASE + 'lists/' + process.env.VUE_APP_MAIL_CHIMP_LIST + '/members', {
-		'email_address': req.query.email,
-		'email_type': '',
-		'status': 'subscribed',
-		'merge_fields': {},
-		'interests': {},
-		'language': '',
-		'vip': false,
-		'location': {
-			'latitude': 0,
-			'longitude': 0
-		},
-		'marketing_permissions': [],
-		'ip_signup': '',
-		'timestamp_signup': '',
-		'ip_opt': '',
-		'timestamp_opt': '',
-		'tags': []
-	}, {
+	const mailchimp = await axios.post(process.env.VUE_APP_MAIL_CHIMP_API_BASE + 'lists/' + process.env.VUE_APP_MAIL_CHIMP_LIST + '/members', { "email_address": req.query.email, "email_type": "", "status": "subscribed", "merge_fields": {}, "interests": {}, "language": "", "vip": false, "location": { "latitude": 0, "longitude": 0 }, "marketing_permissions": [], "ip_signup": "", "timestamp_signup": "", "ip_opt": "", "timestamp_opt": "", "tags": [] }, {
     headers:{
       'Authorization' : 'Bearer ' + process.env.VUE_APP_MAIL_CHIMP_BEARER
     }
