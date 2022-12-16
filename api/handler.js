4fb8cd8d74908a03d159416ccccd4f98c76bed1b
Camilla Tofani <tech@90days.it>
2022-12-16 19:55:15 +0100

    dsw

--- api/handler.js
+++ api/handler.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-export async function getEmail(req, res) {
+export default async function handler(req, res) {
   console.log('sto chiamando mail chimp')
-	const mailchimp = await axios.get(process.env.VUE_APP_MAIL_CHIMP_API_BASE + 'search-members?query=' + req + '&list_id=' + process.env.VUE_APP_MAIL_CHIMP_LIST, {
+	const mailchimp = await axios.get(process.env.VUE_APP_MAIL_CHIMP_API_BASE + 'search-members?query=' + req.query.email + '&list_id=' + process.env.VUE_APP_MAIL_CHIMP_LIST, {
     headers:{
       'Authorization' : 'Bearer ' + process.env.VUE_APP_MAIL_CHIMP_BEARER
     }
@@ -10,17 +10,4 @@ export async function getEmail(req, res) {
 		.finally(()=> console.log('sono uscito da mailchimp'))
 
   return res.send(mailchimp);
-}
-
-export async function getName(req, res) {
-	console.log('sto chiamando mail chimp')
-	const mailchimp = await axios.get(process.env.VUE_APP_MAIL_CHIMP_API_BASE + 'search-members?query=' + req.query.email + '&list_id=' + process.env.VUE_APP_MAIL_CHIMP_LIST, {
-		headers: {
-			'Authorization': 'Bearer ' + process.env.VUE_APP_MAIL_CHIMP_BEARER
-		}
-	})
-		.then(result => result.data)
-		.finally(() => console.log('sono uscito da mailchimp'))
-
-	return res.send(mailchimp);
 }
